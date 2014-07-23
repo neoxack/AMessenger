afaff2188bf4296831fc76041ca9f9c1e86fbd1c
Dragunov Semyon <neoxack@gmail.com>
2014-07-23 17:24:55 +0400

    Added StylesAndTemplates.xaml

--- AMessanger/Views/ShellView.xaml.cs
+++ AMessanger/Views/ShellView.xaml.cs
@@ -10,9 +10,6 @@ namespace AMessenger
 	{
 		public ShellView()
 		{
-			FontFamily font = new FontFamily(new Uri("pack://application:,,,/Resources/"), "./#Roboto");
-			TextElement.FontFamilyProperty.OverrideMetadata(typeof(TextElement), new FrameworkPropertyMetadata(font));
-			TextBlock.FontFamilyProperty.OverrideMetadata(typeof(TextBlock), new FrameworkPropertyMetadata(font));
 		}
 
 		private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
